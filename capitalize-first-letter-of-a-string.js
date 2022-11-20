7f3414f792b2667e22103e5cf5815934c90577be
semagul <isemagul@gmail.com>
2022-11-20 23:12:44 +0100

    solve kata

--- capitalize-first-letter-of-a-string.js
+++ capitalize-first-letter-of-a-string.js
@@ -10,3 +10,11 @@ This sentence is already capitalized	This sentence is already capitalized
 JavaScript / CoffeeScript: Extend the String prototype with a method capitalize() so you can call it on a string like so: "string".capitalize(). Learn about inheritance and the prototype chain.
 
 Furthermore, the built-in string methods toUpperCase() and toLowerCase() are disabled for this Kata. */
+
+String.prototype.capitalize = function() {
+    let code = this.charCodeAt(0)
+    if (code > 96 && code < 122) {
+      code -= 32
+    }
+    return String.fromCharCode(code) + this.slice(1)
+  }
