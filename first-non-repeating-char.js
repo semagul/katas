5f9c932c48f146426e3b220368a11806279ebe7c
semagul <isemagul@gmail.com>
2022-11-22 21:45:20 +0100

    solve kata

--- first-non-repeating-char.js
+++ first-non-repeating-char.js
@@ -5,3 +5,24 @@ For example, if given the input 'stress', the function should return 't', since 
 As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
 
 If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests. */
+
+export function firstNonRepeatingLetter({ input }) {
+    const sLow = input.toLowerCase();
+
+    let charsCount = {};
+    for (const char of sLow) {
+        charsCount[char] = charsCount[char] ? charsCount[char] + 1 : 1;
+    }
+
+    for (const char in charsCount) {
+        if (charsCount[char] === 1) {
+            for (const sChar of input) {
+                if (sChar === char || sChar === char.toUpperCase()) {
+                    return sChar;
+                }
+            }
+        }
+    }
+
+    return "";
+}
