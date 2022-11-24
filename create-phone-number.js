93113beec0821dfc9b8cfa410824342fc21fd7a5
semagul <isemagul@gmail.com>
2022-11-24 23:10:01 +0100

    add solution II

--- create-phone-number.js
+++ create-phone-number.js
@@ -9,3 +9,9 @@ Don't forget the space after the closing parentheses! */
 function createPhoneNumber(n) {
     return '(' + n[0] + n[1] + n[2] + ')' + ' ' + n[3] + n[4] + n[5] + '-' + n[6] + n[7] + n[8] + n[9]
 }
+
+// solution II
+
+function createPhoneNumber(numbers) {
+    return numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
+}
