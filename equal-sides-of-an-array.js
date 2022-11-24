394898bd29c25cf20af564467b07619a00c131b6
semagul <isemagul@gmail.com>
2022-11-24 22:58:58 +0100

    add solution II

--- equal-sides-of-an-array.js
+++ equal-sides-of-an-array.js
@@ -45,3 +45,17 @@ function sum(array) {
     })
     return acc;
 }
+
+
+// solution II
+
+function findEvenIndex(arr) {
+    let left = 0;
+    let right = arr.reduce((s, n) => s + n, 0);
+    for (let i = 0; i < arr.length; i++) {
+        right -= arr[i];
+        if (left === right) return i;
+        left += arr[i];
+    }
+    return -1;
+}
