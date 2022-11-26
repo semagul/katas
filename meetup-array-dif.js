531ebccbdb061f22d36f804e45f35c3b9e32f196
semagul <isemagul@gmail.com>
2022-11-26 19:16:50 +0100

    solve kata

--- meetup-array-dif.js
+++ meetup-array-dif.js
@@ -6,3 +6,16 @@ arrayDiff([1,2],[1]) == [2]
 If a value is present in b, all of its occurrences must be removed from the other:
 
 arrayDiff([1,2,2,2,3],[2]) == [1,3] */
+
+
+function arrayDiff(array1, array2) {
+    if (array1.length === 0) { return [] }
+    if (array2.length === 0) { return array1 }
+
+    let returnArray = [];
+    array1.forEach(function included(element) {
+        if (!array2.includes(element)) { returnArray.push(element) }
+    })
+    
+    return returnArray;
+}
