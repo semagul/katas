939af7e3f2c1a43a52bc45df5fc932cb138e5f7a
semagul <isemagul@gmail.com>
2022-11-27 21:57:16 +0100

    add solution

---  find-the-odd-int.js
+++  find-the-odd-int.js
@@ -12,10 +12,14 @@ Examples
 
 function findOdd(arr) {
     for (let i = 0; i < arr.length; i++) {
-        const count = arr.filter(value => value === arr[i]).length;
+        const count = arr.filter(value => value === arr[i]).length
         if (count % 2 == 1) {
-            return arr[i];
+            return arr[i]
         }
     }
     return -1;
 }
+
+// solution II
+
+const findOdd = (xs) => xs.reduce((a, b) => a ^ b)
