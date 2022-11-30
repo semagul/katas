82c720dde054fd8dcf330f478402c46422c91cc2
semagul <isemagul@gmail.com>
2022-11-30 21:56:09 +0100

    delete kata

--- 12-sleepy-bits.js
+++
@@ -1,6 +0,0 @@
-/* Puzzle
-12 bits, some are sleeping, some are awake. They are all tired and yearn for some sleep. One of them has to stay awake while all others can go to sleep. Can you figure out which one stays awake?
-
-Hints
-why exactly 12 bits? (perhaps there are realtime applications for this)
-fixed test cases will show some patterns and additional hints */
